6d57236f826746ebe234f8228120a6a4ba1f2c98
lewis.wilson <lewis.j.wilson@outlook.com>
2020-09-22 21:44:09 +0900

    Touch Controls Functional

--- Assets/PaddleScript.cs
+++ Assets/PaddleScript.cs
@@ -23,8 +23,28 @@ public class PaddleScript : MonoBehaviour
             return;
         }
 
-        float horizontal = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+        Vector2 movement = Vector2.zero; //initialize direction as zero
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.position.x < Screen.width/2) //if touch is left side of screen
+            {
+                Debug.Log("LeftTouch");
+                movement = Vector2.left; //movement direction = left
+           
+
+            } else if(touch.position.x > Screen.width/2)
+            {
+                Debug.Log("RightTouch");
+                movement = Vector2.right; //movement direction = right
+            }
+
+            transform.Translate(movement * Time.deltaTime * speed, 0); //y axis = 0
+
+        }
+
 
         //setting boundaries for the paddle (screen edges)
         if(transform.position.x < leftScreenEdge)
